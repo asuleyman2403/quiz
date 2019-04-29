5a29c6dbb6505a8ed2c306f9c6bfc0e7a250a4e4
asuleyman2403 <asuleyman2403@gmail.com>
2019-04-30 03:55:52 +0600

    quiz

---
+++ quiz-front/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {ClassProvider, NgModule} from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ProviderService} from './services/provider.service';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from './AuthInterceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    ProviderService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    } as ClassProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
